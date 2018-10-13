a55cfbaa78e0e12c70e7023f5c49a2cf4d424aab
Mesi Rendon <amrendonsa@unal.edu.co>
2018-10-13 09:59:30 -0500

    Initializing contract

--- src/store/modules/course.js
+++ src/store/modules/course.js
@@ -1,8 +1,41 @@
-const state = {}
+import * as constants from '@/store/constants'
+import CourseContract from '@/contracts/Certifier.json'
 
-const actions = {}
+const state = {
+  provider: !!(window.web3 && window.web3.currentProvider),
+  isOwner: false,
+  contract: null,
+  coinbase: null
+}
 
-const mutations = {}
+const actions = {
+  [constants.COURSE_INIT]: ({commit}) => {
+    const abi = CourseContract.abi
+    const contractAddress = '0x11589f14727997Df90f7668f4f095AAC0fA645FE'
+    const contract = web3.eth.contract(abi).at(contractAddress)
+    commit(constants.COURSE_SET_CONTRACT, contract)
+    web3.eth.getCoinbase((error, coinbase) => {
+      if(error) console.error(error)
+      commit(constants.COURSE_SET_COINBASE, coinbase)
+      contract.isOwner({from: coinbase}, (error, isOwner) => {
+        if(error) console.error(error)
+        commit(constants.COURSE_SET_IS_OWNER, isOwner)
+      })
+    })
+  }
+}
+
+const mutations = {
+  [constants.COURSE_SET_CONTRACT]: (state, contract) => {
+    state.contract = contract
+  },
+  [constants.COURSE_SET_COINBASE]: (state, coinbase) => {
+    state.coinbase = coinbase
+  },
+  [constants.COURSE_SET_IS_OWNER]: (state, isOwner) => {
+    state.isOwner = isOwner
+  }
+}
 
 const getters = {}
 
